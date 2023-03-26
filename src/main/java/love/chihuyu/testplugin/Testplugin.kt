7e201cfc6d4dda29b737a91192ebba1de0af6b4d
Rain022 <pikachuureimu@gmail.com>
2023-03-27 01:28:18 +0900

    Initial commit

---
+++ src/main/java/love/chihuyu/testplugin/Testplugin.kt
@@ -0,0 +1,13 @@
+package love.chihuyu.testplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Testplugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
