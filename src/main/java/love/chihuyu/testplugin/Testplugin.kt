528638591195eec77df4b1e4af79e68f8e00f704
Rain022 <pikachuureimu@gmail.com>
2023-03-27 02:20:48 +0900

    結果にコミット

--- src/main/java/love/chihuyu/testplugin/Testplugin.kt
+++ src/main/java/love/chihuyu/testplugin/Testplugin.kt
@@ -1,13 +1,26 @@
 package love.chihuyu.testplugin
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Testplugin : JavaPlugin() {
     override fun onEnable() {
+        println("ずずだぜが発狂します")
         // Plugin startup logic
+        val player = Bukkit.getPlayer("hirosukt")
+
+        if  (player == null) {
+            return
+        }
+        player?.sendMessage("ずずだぜは好きですか？？")
+        val scheduler = Bukkit.getScheduler()
+        scheduler.runTaskTimer(this, Runnable {
+            player?.sendMessage("このメッセージは10秒ごとに表示するぞ")
+        }, 20 * 15, 20 * 10)
     }
 
     override fun onDisable() {
+        println("ずずだぜの発狂が終わりました")
         // Plugin shutdown logic
     }
 }
